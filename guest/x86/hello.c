84cabecc725b470b64bd040517b5f0a8f22adb8f
yuhong.tao@intel.com <yuhong.tao@intel.com>
2019-03-29 20:06:51 +0800

    test

---
+++ guest/x86/hello.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2010 Siemens AG
+ * Author: Jan Kiszka
+ *
+ * Released under GPLv2.
+ */
+
+#include "libcflat.h"
+
+int main(int ac, char **av)
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
