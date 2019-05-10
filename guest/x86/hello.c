6c26944bb39a22649913932c38ee8eb5424456bf
yuhong.tao@intel.com <yuhong.tao@intel.com>
2019-05-10 17:55:33 +0800

    test do_ring3()

--- guest/x86/hello.c
+++ guest/x86/hello.c
@@ -6,10 +6,19 @@
  */
 
 #include "libcflat.h"
+#include "processor.h"
+#include "desc.h"
+
+static void test_func(const char *arg)
+{
+	printf("%s CS:%x\n", arg, read_cs());
+}
 
 int main(int ac, char **av)
 {
 	printf("Hello, World!\n");
-
+	setup_idt();
+	init_do_ring3();
+	do_ring3(test_func, "hahahaha");
 	return 0;
 }
