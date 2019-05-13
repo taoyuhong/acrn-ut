87f22cec8982916faaf2c1b2642b662a5d0c0945
yuhong.tao@intel.com <yuhong.tao@intel.com>
2019-05-13 22:18:07 +0800

    test call gate

--- guest/x86/hello.c
+++ guest/x86/hello.c
@@ -14,6 +14,15 @@ void test_fun(void)
 	printf("%s CS:%x\n", __FUNCTION__, read_cs());
 }
 
+void r3_run(const char *arg)
+{
+	printf("%s CS:%x\n", __FUNCTION__, read_cs());
+		
+	asm volatile ("lcall $0x50, $0");
+	//asm volatile ("int $0x21");
+	printf("after lcall %s CS:%x\n", __FUNCTION__, read_cs());
+}	
+
 int main(int ac, char **av)
 {
 	call_gate_t *gate = (void *)&gdt32[10];
@@ -22,12 +31,15 @@ int main(int ac, char **av)
 	setup_vm();
 	setup_idt();
 	setup_alt_stack();
+	init_do_ring3();
+
 	printf("Hello, World!\n");
 
 	gdt32[11] = gdt32[1];
 	//gdt32[11].access |= 0x40; /* DPL = 2 */ 	
-	gdt32[11].access |= 0x04; /* conforming */ 	
+	//gdt32[11].access |= 0x04; /* conforming */ 	
 	printf("access:%x\n",  gdt32[11].access);
+
 	gate->offset_low = offset & 0xffff;
 	gate->offset_high = (offset & 0xffff0000) >>16;
 	gate->selector = 11 <<3;
@@ -35,9 +47,13 @@ int main(int ac, char **av)
 	gate->type = 0xc;
 	gate->p = 1;
 	gate->system = 0;
-	gate->dpl = 2;
-		
-	asm volatile ("lcall $0x52, $0");
+	gate->dpl = 3;
 
+	//set_idt_entry(0x21, &test_fun, 3);
+		
+	//asm volatile ("lcall $0x52, $0");
+	do_ring3(r3_run, NULL);
+
+	r3_run(NULL);
 	return 0;
 }
