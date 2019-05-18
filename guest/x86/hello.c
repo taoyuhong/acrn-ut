e7ae26479df5cedb5a3b5576b0df085f1fd672d2
yuhong.tao@intel.com <yuhong.tao@intel.com>
2019-05-19 04:20:11 +0800

    test install_read_only_pages()

--- guest/x86/hello.c
+++ guest/x86/hello.c
@@ -6,10 +6,32 @@
  */
 
 #include "libcflat.h"
+#include "desc.h"
+#include "processor.h"
+#include "vm.h"
+
+#define ERROR_ADDR	0x5fff0000
+
+char buf[] = "Hello, this is a memory strings";
 
 int main(int ac, char **av)
 {
-	printf("Hello, World!\n");
+	u32 offset = ((u32)buf) & 0xfff;
+	char *str_read_only = (void *)ERROR_ADDR + offset;
+
+	setup_idt();
+	setup_vm();
+
+
+	//install_pages(phys_to_virt(read_cr3()), (u32)buf, 4096, (void *)ERROR_ADDR);
+	install_read_only_page(phys_to_virt(read_cr3()), ((u32)buf) & 0xfffff000, (void *)ERROR_ADDR);
+
+	printf("read_only:%s\n", str_read_only);
+
+	str_read_only[0] = 'h';
+
+	/* will #PF */
+	printf("read_only:%s\n", str_read_only);
 
 	return 0;
 }
